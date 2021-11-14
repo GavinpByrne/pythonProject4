a30c78a83512e7bec3a9255a4ce6a476deb18c61
GavinPByrne <gavin.byrne@stacked.ie>
2021-11-14 14:21:59 +0000

    Dataframes basics 20211114

--- 4.Gb.py
+++ 4.Gb.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(r"C:\Users\Stackedadmin\Desktop\Flash_test_pandas.csv", encoding='iso8859_2')
+df = pd.read_csv(r"C:\Users\Stackedadmin\Desktop\Flash_test_pandas.csv", encoding='iso8859_2')#would not work without encoding possibly due to Euro signs
 # print(df.head())
 # print(df.info())
 # print (df.describe())
@@ -15,5 +15,13 @@ df = pd.read_csv(r"C:\Users\Stackedadmin\Desktop\Flash_test_pandas.csv", encodin
 # print(df['Colour_Usage'] > 50)
 # print(df[df['Colour_Usage'] > 50])
 # print(df[df['code'] == 'FLOMPS002'])
-abcplus3=df[(df['code'] == '3DENMPS001') |(df['code'] =='ABCMPS001')]#| is or operator and & is and operator
-print(abcplus3)
+# abcplus3 = df[(df['code'] == '3DENMPS001') | (df['code'] == 'ABCMPS001')]  # | is or operator and & is and operator
+# print(abcplus3.head())
+#df['Colour_Cost'] = df.astype({'Colour_Cost':float})
+#print(df.info)
+#print(df['Colour_Cost']*1.04)
+df['Colour_Cost'] = df['Colour_Cost']*1.04#just increases amount bu 1.04
+df['Colour_Cost_Increase'] = df['Colour_Cost']*1.15#adds column with new amount
+print(df.head())
+
+
