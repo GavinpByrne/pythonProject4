7554df2d611e773947ef5ed4a2bf778970831267
GavinPByrne <gavin.byrne@stacked.ie>
2021-11-22 11:41:20 +0000

    Dataframes basics 20211114

---
+++ Run_Rate_test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(r'C:\Users\Stackedadmin\Desktop\Python\Flash01.csv', encoding='ISO-8859-1')
+
+df.loc[df['Mono_Charge'] < 0, 'Drop_Below_Zero'] = 0
+df.loc[df['Mono_Charge'] > 0, 'Drop_Below_Zero'] = df[
+    'Mono_Charge']  # Basically like a IF statement-Creates new column taking out <0
+print(df.head())
+df['Mono_Charge'].sum() / 15 * 21 + (df['Colour_Charge'].sum() / 15 * 21)
+
+df.loc[:, 'Row_Total'] = df.sum(numeric_only=True, axis=1)#
+df.loc['Colum_Total'] = df.sum(numeric_only=True,axis=0)
+df.loc['FLash_Figure'] = df['Mono_Charge'].sum() / 15 * 21 + (df['Colour_Charge'].sum() / 15 * 21)
+print(df.tail())
+df.to_csv(r'C:\Users\Stackedadmin\Desktop\Python\Amended_File.csv')
