2bb5c2f6d46b976932194a97ce91a7a147f86d03
GavinPByrne <gavin.byrne@stacked.ie>
2021-11-22 12:49:44 +0000

    Dataframes basics 20211114

--- Run_Rate_test.py
+++ Run_Rate_test.py
@@ -9,8 +9,11 @@ df.loc[df['Mono_Charge'] > 0, 'Drop_Below_Zero'] = df[
 print(df.head())
 df['Mono_Charge'].sum() / 15 * 21 + (df['Colour_Charge'].sum() / 15 * 21)
 
-df.loc[:, 'Row_Total'] = df.sum(numeric_only=True, axis=1)#
-df.loc['Colum_Total'] = df.sum(numeric_only=True,axis=0)
+# df.loc[:, 'Row_Total'] = df.sum(numeric_only=True, axis=1)#Creates New Column with row total
+# df.loc['Colum_Total'] = df.sum(numeric_only=True,axis=0)#CReates new Column with Column Total
 df.loc['FLash_Figure'] = df['Mono_Charge'].sum() / 15 * 21 + (df['Colour_Charge'].sum() / 15 * 21)
 print(df.tail())
-df.to_csv(r'C:\Users\Stackedadmin\Desktop\Python\Amended_File.csv')
+# df.to_csv(r'C:\Users\Stackedadmin\Desktop\Python\Amended_File.csv')
+# print(df.info())
+print(df.groupby('code')['Colour_Charge'].agg([np.mean, np.max]))
+print(df.pivot_table(values='code', index='Colour_Charge', columns='NAME', aggfunc=np.median))
